bdaa4399e040c6564d6b34df840a94bd181aaa25
Artem Vladimirsky <atremkoto@gmail.com>
2021-08-02 16:52:37 +0300

    Сделал страничку dialogs
    Изучил маршрутизацию, подключил страничку dialogs в приложение

--- src/App.js
+++ src/App.js
@@ -3,15 +3,21 @@ import './App.css';
 import Profile from './components/Profile/Profile';
 import Header from './components/Header/Header'
 import Nav from './components/Nav/Nav';
+import Dialogs from './components/Dialogs/Dialogs';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 const App = () => {
   return (
-
-    <div className="app-wrapper">
-      <Header />
-      <Nav />
-      <Profile />
-    </div>
+    <BrowserRouter>
+      <div className="app-wrapper">
+        <Header />
+        <Nav />
+        <div className="app-wrapper-content">
+          <Route path="/profile" component={Profile} />
+          <Route path="/dialogs" component={Dialogs} />
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -4,8 +4,8 @@ import s from './Nav.module.scss';
 const Nav = () => {
     return (
         <nav className={s.nav}>
-            <a className={s.item}>Profile</a>
-            <a className={s.item}>Messages</a>
+            <a className={s.item} href="/profile">Profile</a>
+            <a className={s.item} href="/dialogs">Messages</a>
             <a className={s.item}>News</a>
             <a className={s.item}>Music</a>
             <a className={s.item}>Settings</a>
