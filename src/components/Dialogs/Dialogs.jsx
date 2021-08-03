01e12652644b3ef43dc1c4d64e456e88f36e9975
Artem Vladimirsky <atremkoto@gmail.com>
2021-08-03 21:24:03 +0300

    Сверстал страничку диалогов

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,9 +3,34 @@ import s from './Dialogs.module.scss';
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.item + ' ' + s.active}>
+                    Artem
+                </div>
+                <div className={s.item}>
+                    Butuz
+                </div>
+                <div className={s.item}>
+                    Sanchez
+                </div>
+                <div className={s.item}>
+                    Seriy
+                </div>
+                <div className={s.item}>
+                    Egor
+                </div>
+                <div className={s.item}>
+                    Andrey
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>How is your it-kamasutra&</div>
+                <div className={s.message}>Yo</div>
+            </div>
         </div>
+    
     );
 }
 
