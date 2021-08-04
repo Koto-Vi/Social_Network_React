81196a4aa857745be765a4457e5b015fa178a38d
Artem Vladimirsky <atremkoto@gmail.com>
2021-08-04 17:34:05 +0300

    Убрал hardcode значения из dialogs, myposts и profile

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -19,20 +19,36 @@ const Message = (props) => {
 }
 
 const Dialogs = (props) => {
+
+let dialogsData = [
+    { id: 1, name: "Artem"},
+    { id: 2, name: "Butuz"},
+    { id: 3, name: "Sanchez"},
+    { id: 4, name: "Seriy"},
+    { id: 5, name: "Egor"},
+    { id: 6, name: "Andrey"}
+];
+
+let messagesData = [
+    { id: 1, message: 'Hi'},
+    { id: 2, message: 'How is your it-kamasutra?'},
+    { id: 3, message: 'Yo'},
+];
+
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <DialogItem name='Artem' id='1' />
-                <DialogItem name='Butuz' id='2' />
-                <DialogItem name='Sanchez' id='3' />
-                <DialogItem name='Seriy' id='4' />
-                <DialogItem name='Egor' id='5' />
-                <DialogItem name='Andrey' id='6' />
+                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id} />
+                <DialogItem name={dialogsData[1].name} id={dialogsData[1].id} />
+                <DialogItem name={dialogsData[2].name} id={dialogsData[2].id} />
+                <DialogItem name={dialogsData[3].name} id={dialogsData[3].id} />
+                <DialogItem name={dialogsData[4].name} id={dialogsData[4].id} />
+                <DialogItem name={dialogsData[5].name} id={dialogsData[5].id} />
             </div>
             <div className={s.messages}>
-                <Message message='Hi' />
-                <Message message='How is your it-kamasutra?' />
-                <Message message='Yo' />
+                <Message message={messagesData[0].message} />
+                <Message message={messagesData[1].message} />
+                <Message message={messagesData[2].message} />
             </div>
         </div>
 
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,6 +3,12 @@ import s from './MyPosts.module.scss';
 import Post from './Post/Post';
 
 const MyPosts = () => {
+
+    let postsData = [
+        { id: 1, message: 'Hi, how are you?', likeCount: 15},
+        { id: 2, message: "It's my first post", likeCount: 20}
+    ];
+
     return (
         <div className={s.postsBlock}>
             My Posts
@@ -14,8 +20,8 @@ const MyPosts = () => {
                 <button>Add post</button>
                 </div>
             </div>
-            <Post message="Hi, how are you?" likeCount="15" />
-            <Post message="It's my first post" likeCount="20"/>
+            <Post message={postsData[0].message} likeCount={postsData[0].likeCount} />
+            <Post message={postsData[1].message} likeCount={postsData[1].likeCount} />
         </div>
     );
 }
