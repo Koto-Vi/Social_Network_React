cce4abb507cf2a656084bb5d515021b9d1d0fd18
Artem Vladimirsky <atremkoto@gmail.com>
2021-08-08 22:57:20 +0300

    Сделал возможность писать посты

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -9,6 +9,7 @@ const MyPosts = (props) => {
     let addPost = () => {
         let text = newPostElement.current.value;
         props.addPost(text);
+        newPostElement.current.value = '';
     };
 
     let postsElements = props.posts.map(post => <Post message={post.message} likeCount={post.likeCount} />);
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,3 +1,5 @@
+import { rerenderEntireTree } from '../render';
+
 let state = {
     profilePage: {
         posts: [
@@ -36,8 +38,8 @@ export let addPost = (postMessage) => {
         message: postMessage,
         likeCount: 0
     };
-
     state.profilePage.posts.push(newPost);
+    rerenderEntireTree(state);
 }
 
 export default state;
